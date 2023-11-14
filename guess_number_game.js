7acfc071de45dee46b072df69e0e79aa4069d238
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2023-11-14 21:16:38 +0530

    Add files via upload

---
+++ guess_number_game.js
@@ -0,0 +1,7 @@
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 100);
+
+function checkGuess() {
+ 
+}

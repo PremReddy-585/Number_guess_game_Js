b394428d7d8a40d0036494d089ab6553926d889c
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2023-11-15 18:14:59 +0530

    Update guess_number_game.js

--- guess_number_game.js
+++ guess_number_game.js
@@ -1,7 +1,26 @@
 let gameResult = document.getElementById("gameResult");
 let userInput = document.getElementById("userInput");
 let randomNumber = Math.ceil(Math.random() * 100);
+console.log(randomNumber);
 
 function checkGuess() {
- 
+    let guessedNumber = parseInt(userInput.value);
+
+    if (guessedNumber > randomNumber) {
+        gameResult.textContent = "Too High, try Again";
+        gameResult.style.background = "#1e217c";
+    }
+    else if (guessedNumber < randomNumber) {
+        gameResult.textContent = "Too Low, try Again";
+        gameResult.style.background = "#1e217c";
+    }
+    else if (guessedNumber === randomNumber) {
+        gameResult.textContent = "Congratulations, You got it Right";
+        gameResult.style.background = "#green";
+}
+else{
+    gameResult.textContent = "Please Provide, a Valid Input";
+        gameResult.style.background = "red";
+    
+}
 }
